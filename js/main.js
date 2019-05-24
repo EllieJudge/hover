0b76d76bc00495edbb0a34f0915c997da8c1b5a2
EllieJudge <judge_ellie@hotmail.com>
2019-05-24 19:04:30 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.getElementById("domExample").innerHTML="This is an example!"; //this made a message through js rather than
+//through html
+
+
+document.getElementById("domSecondExample").innerHTML="This is the second example"; //and again
+
+
+
+//document.getElementsByClassName("className").innerHTML="This is a class";   //attempts to do same but with classes
+//document.getElementById("className").classList.add('MyClass').innerHTML = "Has this worked?";
+
+
+
+document.getElementById("button").addEventListener("click", function() {
+alert("yay you clicked");}) 
+
+
+
+
+
+
